0e22362c7a58e581161c92da933e9c402cee9bf8
SOW Cheikh Omar <cheikhomar60@gmail.com>
2019-03-04 00:38:14 +0000

    some refactoring

---
+++ frontend/src/app/services/base.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+export class BaseService {
+  token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU1MTc0MzMxN30.dnzEs74jbDKHejXU8Q_bndkFzEgGdAb_Wg_iEqCHJul9he3bXuTdgrlieH_C1ZQZsWfHBZ_cmswRHuOslf4sWA`;
+
+  constructor(protected http: HttpClient) {}
+
+  getRequest(url) {
+    return this.http
+      .get<any>(url, {
+        headers: new HttpHeaders()
+          .append("Accept", "application/json")
+          .append("Content-Type", "application/json")
+          .append("Authorization", "Bearer " + this.token),
+        responseType: "json"
+      })
+      .pipe(map((response: Response) => <any>response));
+  }
+
+  postRequest(url, params) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.token });
+    const options = { headers: headers };
+    return this.http.post(url, params, options).subscribe(
+      res => {
+        console.log(res);
+        this.redirect('/users');
+      },
+      err => {
+        console.log(err.message);
+      }
+    );
+  }
+
+  redirect(pathname) {
+    window.location.href = pathname;
+  }
+}
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,71 +3,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/User';
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { BaseService } from "./base.service";
 
 
 @Injectable({
   providedIn: "root"
 })
-export class UserService {
+export class UserService extends BaseService{
 
   baseUrl = 'http://localhost:9090/api';
-  token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU1MTc0MzMxN30.dnzEs74jbDKHejXU8Q_bndkFzEgGdAb_Wg_iEqCHJul9he3bXuTdgrlieH_C1ZQZsWfHBZ_cmswRHuOslf4sWA`;
 
-  constructor(private http: HttpClient) {}
+  constructor(protected http: HttpClient) {
+    super(http);
+  }
 
-  getUsers(): Observable<User[]> {
+  getUsers() {
     const url = `${this.baseUrl}/users`;
-
-    return this.http.get<User[]>(url, {
-      headers:
-        new HttpHeaders()
-          .append('Accept', 'application/json')
-          .append('Content-Type', 'application/json')
-          .append('Authorization', 'Bearer ' + this.token)
-      , responseType: 'json'
-    })
-    .pipe(
-      map((response: Response) => <any>response)
-    );
+    return this.getRequest(url);
   }
 
   getUserByLogin(login) {
     const url = `${this.baseUrl}/users/${login}`;
-
-    return this.http.get<User>(url, {
-      headers:
-        new HttpHeaders()
-          .append('Accept', 'application/json')
-          .append('Content-Type', 'application/json')
-          .append('Authorization', 'Bearer ' + this.token)
-      , responseType: 'json'
-    })
-    .pipe(
-      map((response: Response) => <any>response)
-    );
+    return this.getRequest(url);
   }
 
   createUser(params) {
-    console.log(params);
     const url = `${this.baseUrl}/users`;
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + this.token });
-    const options = { headers: headers };
-    return this.http
-      .post(url, params, options)
-      .subscribe(
-        res =>{
-            console.log(res);
-            this.redirect('/users');
-        },
-        err => {
-            console.log(err.message);
-        }
-    );
-  }
-
-  redirect(pathname) {
-    window.location.href = pathname;
+    return this.postRequest(url, params);
   }
 }
