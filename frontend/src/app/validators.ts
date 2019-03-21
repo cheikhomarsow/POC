31ec9f7e9b85d84fe25ac8d8da2a91f30ce9dda2
SOW Cheikh Omar <cheikhomar60@gmail.com>
2019-03-21 20:16:34 +0000

    add some icons

--- frontend/src/app/validators.ts
+++ frontend/src/app/validators.ts
@@ -5,7 +5,6 @@ export function loginValidator(control: AbstractControl) {
   if (control && (control.value !== null || control.value !== undefined)) {
     const cnfloginValue = control.value;
     if (cnfloginValue.length >= 4) {
-      console.log("===========>");
       // const passValue = passControl.value;
       // if (passValue !== cnfpassValue || passValue === '') {
       //     return {
